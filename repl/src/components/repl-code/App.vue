055bb9a1bfdb7b24822d876c5611393f0ab9fc0f
Luke Diebold <luke@ldiebold.com>
2024-06-25 16:00:04 +1000

    updated repl code

--- repl/src/components/repl-code/App.vue
+++ repl/src/components/repl-code/App.vue
@@ -1,13 +1,17 @@
 <script lang="ts" setup>
 import { useAction } from '@vueaction/core'
-import { ChangePasswordAction, ChangePasswordPayload } from './ChangePasswordAction'
+import { ChangePasswordAction, ChangePasswordOptions, ChangePasswordPayload } from './ChangePasswordAction'
 import { setActions } from './setActions'
 
-// This is for demonstation purposes. Rather than using a real API,
+// NOTE: Vue action requires a driver. This example uses the "javascript" driver
+// with a mock latency of 400ms to help demonstrate the library.
+
+// Since this is for demonstation purposes, rather than using a real API,
 // we map some functions that VueAction can trigger.
 setActions({
-  'change-password'(payload: ChangePasswordPayload) {
-    alert('changing password: ' + JSON.stringify(payload))
+  'change-password'(payload: ChangePasswordPayload, options: ChangePasswordOptions) {
+    alert('changing password: ' + JSON.stringify(payload, null, 2))
+    console.log('options: ', options)
   }
 })
 
@@ -16,11 +20,25 @@ const changePasswordAction = useAction(ChangePasswordAction)
 
 <template>
   <div>
+    <input
+      v-model="changePasswordAction.payload.value.oldPassword"
+      placeholder="old password"
+    />
+
+    <br />
+
+    <input
+      v-model="changePasswordAction.payload.value.newPassword"
+      placeholder="new password"
+    />
+
+    <br />
+
     <button
       @click="changePasswordAction.run()"
-      :disabled="changePasswordAction.running"
+      :disabled="changePasswordAction.running.value"
     >
-      {{ changePasswordAction.running.value ? 'running' : 'run' }}
+      {{ changePasswordAction.running.value ? 'changing...' : 'Change Password' }}
     </button>
   </div>
 </template>
